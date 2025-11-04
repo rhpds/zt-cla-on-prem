e6e53ca9a41fc8c884e9a5baec569b564180ff61
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2025-11-04 13:54:54 -0500

    verify that the ~/.config and ~/.local/bin directories exist in the home directory

--- runtime-automation/module-01/setup-rhel.sh
+++ runtime-automation/module-01/setup-rhel.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 echo "Starting module called module-01" >> /tmp/progress.log
+# verify that the ~/.config and ~/.local/bin directories exist in the home directory
+mkdir ~/.config
+mkdir ~/.local/bin
