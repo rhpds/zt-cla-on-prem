83bb667d3f22bbc92f632aa2fccfae6b1a133b58
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2026-01-28 18:54:40 -0500

    update image

--- runtime-automation/module-02/setup-rhel.sh
+++ runtime-automation/module-02/setup-rhel.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo "Starting module called module-02" >> /tmp/progress.log
 
+rhel-cla start
+
 sed -i 's|quay.io/ramalama/ramalama:latest|quay.io/ramalama/ramalama:6ae863e9f552797b45d0fd8632c4f7ec9b78c430|g' ~/.config/rhel-cla/.env
 
 sed -i 's|registry.redhat.io/rhel-cla/installer-rhel10:latest|registry.redhat.io/rhel-cla/installer-rhel10@sha256:7dc16cb8af1afc1055dc2b293c7e45feebb072b9e3b602e5d008816b2be9fe59|g' ~/.config/rhel-cla/.env
