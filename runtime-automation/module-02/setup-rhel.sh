953a8c2ef09d27b66fa3cd888d6521f6430d4775
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2026-01-28 19:17:32 -0500

    update image

--- runtime-automation/module-02/setup-rhel.sh
+++ runtime-automation/module-02/setup-rhel.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Starting module called module-02" >> /tmp/progress.log
 
-rhel-cla start
+~/.local/bin/rhel-cla start
 
 sed -i 's|quay.io/ramalama/ramalama:latest|quay.io/ramalama/ramalama:6ae863e9f552797b45d0fd8632c4f7ec9b78c430|g' ~/.config/rhel-cla/.env
 
